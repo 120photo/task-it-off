5abc264a2655dbcac4af79b451f548087b4afc28
Jose Juarez <120photo@gmail.com>
2014-09-22 10:54:27 -0700

    added avatar field to users schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921050959) do
+ActiveRecord::Schema.define(version: 20140922015143) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140921050959) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
