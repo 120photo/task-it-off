2683c0bcce1943ca43b5b90d972b3ec0216c1bbc
Jose Juarez <120photo@gmail.com>
2014-09-30 12:40:55 -0700

    added form to list view to create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,27 +1,34 @@
 class ListsController < ApplicationController
+
+  before_filter :authenticate_user!
+
   def index
-    @list = List
+    @list = List.all
     @my_lists = users_list
   end
 
   def show
+    @list = List.all.where(list_id: params[:id])
   end
 
   def new
-    @list = List.all
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    @list.save
+    redirect_to lists_url
   end
 
   def edit
   end
 
-  def create
-    @list = List.all
-  end
-
   private
 
   def list_params
-    params.require(:name)
+    params.require(:list).permit(:name)
   end
 
   def users_list
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   resources :lists
   resources :tasks
-  
+
 end
